b39381ce9038cd58efa805be4e2ba94835858177
teddy <teddyjh@protonmail.com>
2022-09-14 08:04:07 +0200

    Fix UI in Interface Builder, and add properties + methods

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func changeFilter() {}
+    
+    func save() {}
+    
+    func intensityChanged() {}
+    
 }
 
