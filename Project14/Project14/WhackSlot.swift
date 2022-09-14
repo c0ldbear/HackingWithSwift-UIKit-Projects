228a4f8d6b10851d19cc255b6480600ddc7395f1
teddy <teddyjh@protonmail.com>
2022-09-14 13:28:37 +0200

    Add mask to show the penguins inside of the mask

--- Project14/Project14/WhackSlot.swift
+++ Project14/Project14/WhackSlot.swift
@@ -9,6 +9,7 @@ import SpriteKit
 import UIKit
 
 class WhackSlot: SKNode {
+    var charNode: SKSpriteNode!
     // No init() => Use parent init()
     
     func configurate(at position: CGPoint) {
@@ -16,5 +17,17 @@ class WhackSlot: SKNode {
         
         let sprite = SKSpriteNode(imageNamed: "whackHole")
         addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask") // Remember, with crop nodes everything with a color is visible, and everything transparent is invisible, so the whackMask.png will show all parts of the character that are above the hole.
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
     }
 }
