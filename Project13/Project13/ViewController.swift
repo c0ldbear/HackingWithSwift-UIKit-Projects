7caf1876b854aa5abb282f029456e80d1b663367
teddy <teddyjh@protonmail.com>
2022-09-14 08:13:46 +0200

    Add possibility to import image from photo lib

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -7,15 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
+    
+    var currentImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        title = "YACIFP" // Yet Another Core Image Filters Program
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
     }
-
+    
+    @objc func importPicture() {
+        print("importing picture...")
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        present(imagePicker, animated: true)
+    }
+    
     func changeFilter() {}
     
     func save() {}
@@ -24,3 +37,13 @@ class ViewController: UIViewController {
     
 }
 
+// To fulfill the inheritance of UIImagePickerControllerDelegate we need the following:
+extension ViewController {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+       
+        dismiss(animated: true)
+        
+        currentImage = image
+    }
+}
