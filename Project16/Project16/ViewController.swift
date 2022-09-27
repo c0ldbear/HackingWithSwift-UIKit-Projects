c870320c912a0c1f07625fb74f9332d26798abdd
teddy <teddyjh@protonmail.com>
2022-09-27 09:44:57 +0200

    Add action to the i-button

--- Project16/Project16/ViewController.swift
+++ Project16/Project16/ViewController.swift
@@ -24,6 +24,16 @@ class ViewController: UIViewController, MKMapViewDelegate {
         mapView.addAnnotations([london, oslo, paris, rome, washington, stockholm])
     }
 
-
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard let capital = view.annotation as? Capital else { return }
+        
+        let placeName = capital.title
+        let placeInfo = capital.info
+        
+        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
 }
 
