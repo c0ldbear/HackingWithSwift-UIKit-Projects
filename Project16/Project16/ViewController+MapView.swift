d4c9d89aabdd23c8f551c4321731807de0f5918d
teddy <teddyjh@protonmail.com>
2022-09-27 09:39:22 +0200

    Add comments from HackinWithSwift site
    Website where comments are found:
    https://www.hackingwithswift.com/read/16/3/annotations-and-accessory
    -views-mkpinannotationview

--- Project16/Project16/ViewController+MapView.swift
+++ Project16/Project16/ViewController+MapView.swift
@@ -10,25 +10,25 @@ import MapKit
 
 extension ViewController {
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        // 1
+        // 1: If the annotation isn't from a capital city, it must return nil so iOS uses a default view.
         guard annotation is Capital else { return nil }
         
-        // 2
+        // 2: Define a reuse identifier. This is a string that will be used to ensure we reuse annotation views as much as possible.
         let identifier = "Capital"
         
-        // 3
+        // 3: Try to dequeue an annotation view from the map view's pool of unused views.
         var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
         
         if annotationView == nil {
-            // 4
+            // 4: If it isn't able to find a reusable view, create a new one using MKPinAnnotationView and sets its canShowCallout property to true. This triggers the popup with the city name.
             annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier) // Same as MKPinAnnotationView (renamed in iOS 16)
             annotationView?.canShowCallout = true
             
-            // 5
+            // 5: Create a new UIButton using the built-in .detailDisclosure type. This is a small blue "i" symbol with a circle around it.
             let btn = UIButton(type: .detailDisclosure)
             annotationView?.rightCalloutAccessoryView = btn
         } else {
-            // 6
+            // 6: If it can reuse a view, update that view to use a different annotation.
             annotationView?.annotation = annotation
         }
         
